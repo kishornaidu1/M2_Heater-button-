609e8662d06b1b40eb693de691aaed9bf6c95e59
kishornaidu1 <77820389+kishornaidu1@users.noreply.github.com>
2022-04-20 22:35:11 +0530

    Add files via upload

---
+++ 3_Implementation/main.c
@@ -0,0 +1,30 @@
+/**
+ * @file act1.c
+ * @author athulya43(you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
+#include "activity4.h"
+#include<avr/io.h>
+#include<util/delay.h>
+
+uint16_t temperature;
+initADC();
+char temp;
+
+int main(void)
+{
+  while(1)
+  {
+    temp=ReadADC(0);
+    _delay_ms(10);
+  }
+  return 0;
+}
